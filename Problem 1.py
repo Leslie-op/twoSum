d41fac358eb711e3620775f7d7f952ea7e498d01
Anas <anasabdullah460@gmail.com>
2024-11-07 21:08:40 +0500

    Update Problem 1.py

--- Problem 1.py
+++ Problem 1.py
@@ -1,13 +1,13 @@
-class solution(object)
-    def twoSum(self , numbers , target):
-        self.numbers = numbers 
-        self.target = target
-        for solution in range(numbers):
-            if solution[0] + solution[1] = target
-               print([solution[0] + solution[1]])
-            else:
-                print("error")
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-numbers = [7 , 8 , 9]
-target = 15
-result = twoSum(numbers , target)
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
